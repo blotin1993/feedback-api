3e753f557c18659768c60d7104ae027f5f471772
blotin1993 <blottagonzalo@gmail.com>
2021-03-04 12:39:37 -0300

    corrected sync issues

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"log"
 
 	"github.com/blotin1993/feedback-api/db"
 	//"github.com/vincent/feedback-api/handler"
-=======
 	"fmt"
 
->>>>>>> bf396c6bb10b12fa2166fd70e5637890ebbfa72e
 	"github.com/joho/godotenv"
 )
 
@@ -17,7 +14,6 @@ import (
 var MyEnv map[string]string
 
 func main() {
-<<<<<<< HEAD
 
 	if db.CheckConnection() == 0 {
 		log.Fatal("No connection to the BD")
@@ -27,12 +23,8 @@ func main() {
 	//handler.handlers()
 
 	myEnv, err := godotenv.Read()
-	s3Bucket := MyEnv["S3_BUCKET"]
-=======
-	MyEnv, err := godotenv.Read()
 	if err == nil {
 		dbURI := MyEnv["DB_URI"]
 		fmt.Println(dbURI)
 	}
->>>>>>> bf396c6bb10b12fa2166fd70e5637890ebbfa72e
 }
