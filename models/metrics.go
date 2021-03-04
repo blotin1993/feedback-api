490b9d27d2f1f18ce99f6bc2c78ebbf92c271fb1
Andrea Juarez <Andre@Andreas-MacBook-Air.local>
2021-03-04 17:57:20 -0300

    remove commented code

--- models/metrics.go
+++ models/metrics.go
@@ -8,34 +8,3 @@ type Metrics struct {
 	Id 			primitive.ObjectID  `bson:"_id,omitempty" json:"id"`
 	Text 		string     			`bson:"text,omitempty" json:"text"`
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-Métricas de Feedback:
- 1 - Let´s work on this.
- 2 - Reach the Goal.
- 3 - Relevant Performance.
- 4 - Master.
-*/
